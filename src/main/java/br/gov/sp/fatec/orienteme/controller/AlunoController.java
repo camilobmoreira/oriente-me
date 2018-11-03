14e1c47ea134b6a34f05cd4d3f1fd1343c55bf86
unknown <josejonatasoliveira.jjo@gmail.com>
2018-11-03 09:23:10 -0300

    AlunoController | buscarAluno

--- src/main/java/br/gov/sp/fatec/orienteme/controller/AlunoController.java
+++ src/main/java/br/gov/sp/fatec/orienteme/controller/AlunoController.java
@@ -1,10 +1,28 @@
 package br.gov.sp.fatec.orienteme.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatec.orienteme.model.Aluno;
+import br.gov.sp.fatec.orienteme.service.AlunoService;
 
 @RestController
 @RequestMapping(value = "/aluno")
 public class AlunoController {
+	
+	@Autowired
+	private AlunoService alunoService;
+	
+	@RequestMapping(value = "/{nome}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<List<Aluno>> buscarAluno(@PathVariable String nome){
+		return ResponseEntity.ok(alunoService.pesquisarAluno(nome));
+	}
 
 }
