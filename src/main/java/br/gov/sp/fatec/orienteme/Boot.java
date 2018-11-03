8e84414af18023582e4a444910f3a8dffaba496e
unknown <josejonatasoliveira.jjo@gmail.com>
2018-11-03 10:02:45 -0300

    Configuracao SpringBoot

--- src/main/java/br/gov/sp/fatec/orienteme/Boot.java
+++ src/main/java/br/gov/sp/fatec/orienteme/Boot.java
@@ -2,28 +2,26 @@ package br.gov.sp.fatec.orienteme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@Controller
 @Configuration
+@EntityScan(basePackages="br.gov.sp.fatec.orienteme")
+@EnableJpaRepositories(basePackages="br.gov.sp.fatec.orienteme.repository")
 @EnableSwagger2
 public class Boot
 {
 
-   public static void main(String[] args)
-   {
-      SpringApplication.run(Boot.class, args);
-   }
-
-   @GetMapping("/")
-   @ResponseBody
-   public String home()
-   {
-      return "home";
-   }
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Configuration.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Configuration.class, args);
+	}
 }
